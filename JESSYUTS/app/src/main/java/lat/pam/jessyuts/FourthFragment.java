04f21c7f4864fbb764aa0c40130f9f637ce1faec
Jessy Faujiyyah Khairani <jessyfaujiyyah06@gmail.com>
2022-11-06 17:26:42 +0700

    first

---
+++ JESSYUTS/app/src/main/java/lat/pam/jessyuts/FourthFragment.java
@@ -0,0 +1,40 @@
+package lat.pam.jessyuts;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import lat.pam.jessyuts.databinding.FragmentFourthBinding;
+
+public class FourthFragment extends Fragment {
+
+    private FragmentFourthBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFourthBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
